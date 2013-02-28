220d6a407961e2049b95433e616374de6f7ddcd2
Maikel vd H <ma1kelvdh@gmail.com>
2013-02-28 22:32:59 +0100

    finish features to read predetermined alliances and spawn locations from INI

--- src/multiplayer_allies_fix.asm
+++ src/multiplayer_allies_fix.asm
@@ -1,21 +1,119 @@
-;@HOOK	0x0053DC0B	_Read_Scenario_INI_Ally_Test
+@HOOK	0x0053DC0B	_Read_Scenario_INI_Ally_Test
+
+str_HouseAllyOne 		db "HouseAllyOne",0
+str_HouseAllyTwo 		db "HouseAllyTwo",0
+str_HouseAllyThree 		db "HouseAllyThree",0
+str_HouseAllyFour 		db "HouseAllyFour",0
+str_HouseAllyFive 		db "HouseAllyFive",0
+str_HouseAllySix 		db "HouseAllySix",0
+str_HouseAllySeven	 	db "HouseAllySeven",0
+
+str_Multi1_Alliances	db "Multi1_Alliances",0
+str_Multi2_Alliances	db "Multi2_Alliances",0
+str_Multi3_Alliances	db "Multi3_Alliances",0
+str_Multi4_Alliances	db "Multi4_Alliances",0
+str_Multi5_Alliances	db "Multi5_Alliances",0
+str_Multi6_Alliances	db "Multi6_Alliances",0
+str_Multi7_Alliances	db "Multi7_Alliances",0
+str_Multi8_Alliances	db "Multi8_Alliances",0
+
+%define HOUSE_MULTI1 0x0c
+%define HOUSE_MULTI2 0x0d
+%define HOUSE_MULTI3 0x0e
+%define HOUSE_MULTI4 0x0f
+%define HOUSE_MULTI5 0x10
+%define HOUSE_MULTI6 0x11
+%define HOUSE_MULTI7 0x12
+%define HOUSE_MULTI8 0x13
+
+; args <HouseType that will ally>, <HouseType to ally>
+%macro Ally_House 2
+	mov		eax, %1 ; House that will ally another house
+	call	0x004D2CB0 ; HouseClass::As_Pointer()
+	mov		edx, %2	; House to be allied
+	call	0x004D6060 ; HouseClass::Make_Ally(HousesType)
+%endmacro
+
+; Loads alliances from map for a HouseType
+; args <HouseType>, <INI section string>, <INIClass>
+%macro Load_Alliances 3
+	INI_Get_Int_ %3, %2, str_HouseAllyOne, 0
+	cmp		eax, 0
+	jz		.Dont_Ally_1_%1	
+	mov		edx, eax
+	Ally_House %1, edx
+	
+.Dont_Ally_1_%1:
+	INI_Get_Int_ %3, %2, str_HouseAllyTwo, 0
+	cmp		eax, 0
+	jz		.Dont_Ally_2_%1
+	mov		edx, eax
+	Ally_House %1, edx
+	
+.Dont_Ally_2_%1:
+
+	INI_Get_Int_ %3, %2, str_HouseAllyThree, 0
+	cmp		eax, 0
+	jz		.Dont_Ally_3_%1
+	mov		edx, eax
+	Ally_House %1, edx
+	
+.Dont_Ally_3_%1:
+
+	INI_Get_Int_ %3, %2, str_HouseAllyFour, 0
+	cmp		eax, 0
+	jz		.Dont_Ally_4_%1
+	mov		edx, eax
+	Ally_House %1, edx
+	
+.Dont_Ally_4_%1:
+
+	INI_Get_Int_ %3, %2, str_HouseAllyFive, 0
+	cmp		eax, 0
+	jz		.Dont_Ally_5_%1
+	mov		edx, eax
+	Ally_House %1, edx
+	
+.Dont_Ally_5_%1:
+
+	INI_Get_Int_ %3, %2, str_HouseAllySix, 0
+	cmp		eax, 0
+	jz		.Dont_Ally_6_%1
+	mov		edx, eax
+	Ally_House %1, edx
+	
+.Dont_Ally_6_%1:
+
+	INI_Get_Int_ %3, %2, str_HouseAllySeven, 0
+	cmp		eax, 0
+	jz		.Dont_Ally_7_%1
+	mov		edx, eax
+	Ally_House %1, edx
+	
+.Dont_Ally_7_%1:
+	
+%endmacro
 
 _Read_Scenario_INI_Ally_Test:
 	call    0x0053DED4  ; Assign_Houses(void)
 	
 	Save_Registers
 	
-	; Have Multi1 ally Multi2
-	mov		eax, 0x0c ; Multi1
-	call	0x004D2CB0 ; HouseClass::As_Pointer()
-	mov		edx, 0x0d	; Multi 2
-	call	0x004D6060 ; HouseClass::Make_Ally(HousesType)
+	lea		esi, [ebp-140]
+	; args <HouseType>, <INI section string>, <INIClass>
+	Load_Alliances HOUSE_MULTI1, str_Multi1_Alliances, esi
+	Load_Alliances HOUSE_MULTI2, str_Multi2_Alliances, esi
+	Load_Alliances HOUSE_MULTI3, str_Multi3_Alliances, esi
+	Load_Alliances HOUSE_MULTI4, str_Multi4_Alliances, esi
+	Load_Alliances HOUSE_MULTI5, str_Multi5_Alliances, esi
+	Load_Alliances HOUSE_MULTI6, str_Multi6_Alliances, esi
+	Load_Alliances HOUSE_MULTI7, str_Multi7_Alliances, esi
+	Load_Alliances HOUSE_MULTI8, str_Multi8_Alliances, esi
 	
+	; Have Multi1 ally Multi2
+;	Ally_House HOUSE_MULTI1, HOUSE_MULTI2
 	; Have Multi2 ally Multi1
-	mov		eax, 0x0d ; Multi2
-	call	0x004D2CB0 ; HouseClass::As_Pointer()
-	mov		edx, 0x0c	; Multi 1
-	call	0x004D6060 ; HouseClass::Make_Ally(HousesType)
+;	Ally_House HOUSE_MULTI2, HOUSE_MULTI1
 	
 	Restore_Registers
 	
--- src/selectable_spawn_locations.asm
+++ src/selectable_spawn_locations.asm
@@ -1,4 +1,60 @@
-;@HOOK 0x0053E6A9	_Create_Units_Spawn_Location
+@HOOK 0x0053DDEB	_Read_Scenario_INI_Spawn_Locations
+@HOOK 0x0053E6A9	_Create_Units_Spawn_Location
+
+str_SpawnLocations 		db "SpawnLocations",0
+str_multi1				db "Multi1",0
+str_multi2				db "Multi2",0
+str_multi3				db "Multi3",0
+str_multi4				db "Multi4",0
+str_multi5				db "Multi5",0
+str_multi6				db "Multi6",0
+str_multi7				db "Multi7",0
+str_multi8				db "Multi8",0
+
+multi1_spawn	dd 0
+multi2_spawn	dd 0
+multi3_spawn	dd 0
+multi4_spawn	dd 0
+multi5_spawn	dd 0
+multi6_spawn	dd 0
+multi7_spawn	dd 0
+multi8_spawn	dd 0
+
+_Read_Scenario_INI_Spawn_Locations:
+	push	eax
+	Save_Registers
+	
+	lea     esi, [ebp-140]
+	
+	INI_Get_Int_ esi, str_SpawnLocations, str_multi1, -1
+	mov		DWORD [multi1_spawn], eax
+	
+	INI_Get_Int_  esi, str_SpawnLocations, str_multi2, -1
+	mov		DWORD [multi2_spawn], eax
+	
+	INI_Get_Int_  esi, str_SpawnLocations, str_multi3, -1
+	mov		DWORD [multi3_spawn], eax
+	
+	INI_Get_Int_  esi, str_SpawnLocations, str_multi4, -1
+	mov		DWORD [multi4_spawn], eax
+	
+	INI_Get_Int_  esi, str_SpawnLocations, str_multi5, -1
+	mov		DWORD [multi5_spawn], eax
+	
+	INI_Get_Int_  esi, str_SpawnLocations, str_multi6, -1
+	mov		DWORD [multi6_spawn], eax
+	
+	INI_Get_Int_  esi, str_SpawnLocations, str_multi7, -1
+	mov		DWORD [multi7_spawn], eax
+	
+	INI_Get_Int_  esi, str_SpawnLocations, str_multi8, -1
+	mov		DWORD [multi8_spawn], eax
+	
+	Restore_Registers
+	pop		eax
+	call 	0x0053E204 ; Create_Units(int)
+	jmp		0x0053DDF0
+	
 
 _Create_Units_Spawn_Location:
 ; [ebp-0x30] is spawn location local variable
@@ -10,17 +66,161 @@ _Create_Units_Spawn_Location:
 	cmp		DWORD ebx, 0x0d
 	je		.Spawn_Multi2
 	
+	cmp		DWORD ebx, 0x0e
+	je		.Spawn_Multi3
+	
+	cmp		DWORD ebx, 0x0f
+	je		.Spawn_Multi4
+	
+	cmp		DWORD ebx, 0x10
+	je		.Spawn_Multi5
+	
+	cmp		DWORD ebx, 0x11
+	je		.Spawn_Multi6
+	
+	cmp		DWORD ebx, 0x12
+	je		.Spawn_Multi7
+	
+	cmp		DWORD ebx, 0x13
+	je		.Spawn_Multi8
+	
 .Ret:
 	mov     eax, DWORD [ebp-0x30]
 	mov     word [ebp-0x1C], ax
 	jmp		0x0053E6B0
 	
 .Spawn_Multi1:
-	mov		eax, 0
-	mov		DWORD [ebp-0x30], 0
+	mov		eax, DWORD [multi1_spawn]
+	
+	cmp		eax, DWORD -1
+	jz		.Ret
+	
+	add		eax, eax
+	add		eax, 2
+	add		eax, 0x006678F5 ; Waypoints
+	
+	xor		edi, edi
+	mov		WORD di, [eax]
+	mov		eax, edi
+	
+	mov		DWORD [ebp-0x30], eax
+	jmp		0x0053E6B0
+
+.Spawn_Multi2:
+	mov		eax, DWORD [multi2_spawn]
+	
+	cmp		eax, DWORD -1
+	jz		.Ret
+	
+	add		eax, eax
+	add		eax, 2
+	add		eax, 0x006678F5 ; Waypoints
+	
+	xor		edi, edi
+	mov		WORD di, [eax]
+	mov		eax, edi
+	
+	mov		DWORD [ebp-0x30], eax
 	jmp		0x0053E6B0
 	
-.Spawn_Multi2:
-	mov		eax, 9003
-	mov		DWORD [ebp-0x30], 9003
+.Spawn_Multi3:
+	mov		eax, DWORD [multi3_spawn]
+	
+	cmp		eax, DWORD -1
+	jz		.Ret
+	
+	add		eax, eax
+	add		eax, 2
+	add		eax, 0x006678F5 ; Waypoints
+	
+	xor		edi, edi
+	mov		WORD di, [eax]
+	mov		eax, edi
+	
+	mov		DWORD [ebp-0x30], eax
+	jmp		0x0053E6B0
+	
+.Spawn_Multi4:
+	mov		eax, DWORD [multi4_spawn]
+	
+	cmp		eax, DWORD -1
+	jz		.Ret
+	
+	add		eax, eax
+	add		eax, 2
+	add		eax, 0x006678F5 ; Waypoints
+	
+	xor		edi, edi
+	mov		WORD di, [eax]
+	mov		eax, edi
+	
+	mov		DWORD [ebp-0x30], eax
+	jmp		0x0053E6B0
+	
+.Spawn_Multi5:
+	mov		eax, DWORD [multi5_spawn]
+	
+	cmp		eax, DWORD -1
+	jz		.Ret
+	
+	add		eax, eax
+	add		eax, 2
+	add		eax, 0x006678F5 ; Waypoints
+	
+	xor		edi, edi
+	mov		WORD di, [eax]
+	mov		eax, edi
+	
+	mov		DWORD [ebp-0x30], eax
+	jmp		0x0053E6B0
+	
+.Spawn_Multi6:
+	mov		eax, DWORD [multi6_spawn]
+	
+	cmp		eax, DWORD -1
+	jz		.Ret
+	
+	add		eax, eax
+	add		eax, 2
+	add		eax, 0x006678F5 ; Waypoints
+	
+	xor		edi, edi
+	mov		WORD di, [eax]
+	mov		eax, edi
+	
+	mov		DWORD [ebp-0x30], eax
+	jmp		0x0053E6B0
+	
+.Spawn_Multi7:
+	mov		eax, DWORD [multi7_spawn]
+	
+	cmp		eax, DWORD -1
+	jz		.Ret
+	
+	add		eax, eax
+	add		eax, 2
+	add		eax, 0x006678F5 ; Waypoints
+	
+	xor		edi, edi
+	mov		WORD di, [eax]
+	mov		eax, edi
+	
+	mov		DWORD [ebp-0x30], eax
+	jmp		0x0053E6B0
+	
+.Spawn_Multi8:
+	mov		eax, DWORD [multi8_spawn]
+	
+	cmp		eax, DWORD -1
+	jz		.Ret
+	
+	add		eax, eax
+	add		eax, 2
+	add		eax, 0x006678F5 ; Waypoints
+	
+	xor		edi, edi
+	mov		WORD di, [eax]
+	mov		eax, edi
+	
+	mov		DWORD [ebp-0x30], eax
 	jmp		0x0053E6B0
